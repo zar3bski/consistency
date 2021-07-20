badbbacf60486056d9576fb3c312783334098adf
zar3bski <zarebskidavid@gmail.com>
2021-07-20 18:36:50 +0200

    remove duplicates

--- pkg/pipeline/pipeline_tests.go
+++
@@ -1,7 +0,0 @@
-package pipeline
-
-import "testing"
-
-func TestPipeline(t *testing.T) {
-	p := identifyType(loadFile("test/data_test/collection1/01 - Some-title.mp3"))
-}
--- test/pipeline_test.go
+++
@@ -1,7 +0,0 @@
-package test
-
-import "testing"
-
-func TestPipeline(t *testing.T) {
-	pipeline.loadFile("test/data_test")
-}
